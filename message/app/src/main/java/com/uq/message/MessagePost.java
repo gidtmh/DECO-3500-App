b4afb736e2091bbe7afee4aa713f9656605f935b
Gidtmh <gideontmh@gmail.com>
2014-10-16 10:17:15 +1000

    put in a adapter to try and add picture sharing

--- message/app/src/main/java/com/uq/message/MessagePost.java
+++ message/app/src/main/java/com/uq/message/MessagePost.java
@@ -12,11 +12,16 @@ import com.parse.ParseUser;
 /**
  * Data model for a post.
  */
+
 @ParseClassName("Posts")
 public class MessagePost extends ParseObject {
     public String getText() {
         return getString("text");
     }
+
+    public String getImage(){ return getString("image");}
+
+    public void setImage(String value){ put("text", value); }
 
     public void setText(String value) {
         put("text", value);
